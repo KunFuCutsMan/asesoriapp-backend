107184dfb7a097b383301671f4f68f509d7414e5
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-07-12 11:31:31 -0600

    Admin & Asesor factories

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Admin extends Model
 {
+    use HasFactory;
+
     protected $table = 'admin';
     protected $primaryKey = 'id';
     public $timestamps = false;
